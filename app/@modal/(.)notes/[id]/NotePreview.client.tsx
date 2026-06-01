46aa9d96fafe5b8ebe9619a9ac3f65a180c7fbc9
Volodymyr Lantrat <stivenuaking@gmail.com>
2026-06-01 12:17:49 +0300

    feat: wrap NotePreview component in Modal for improved UI presentation

--- app/@modal/(.)notes/[id]/NotePreview.client.tsx
+++ app/@modal/(.)notes/[id]/NotePreview.client.tsx
@@ -1,7 +1,9 @@
 "use client";
 
 import { useQuery } from "@tanstack/react-query";
+import { useRouter } from "next/navigation";
 import { fetchNoteById } from "@/lib/api";
+import Modal from "@/components/Modal/Modal";
 import css from "./NotePreview.module.css";
 
 interface Props {
@@ -9,6 +11,8 @@ interface Props {
 }
 
 export default function NotePreviewClient({ id }: Props) {
+  const router = useRouter();
+
   const { data: note, isLoading, isError } = useQuery({
     queryKey: ["note", id],
     queryFn: () => fetchNoteById(id),
@@ -19,17 +23,22 @@ export default function NotePreviewClient({ id }: Props) {
   if (isError || !note) return <p>Something went wrong.</p>;
 
   return (
-    <div className={css.container}>
-      <div className={css.item}>
-        <div className={css.header}>
-          <h2>{note.title}</h2>
+    <Modal>
+      <div className={css.container}>
+        <div className={css.item}>
+          <div className={css.header}>
+            <h2>{note.title}</h2>
+          </div>
+          <p className={css.tag}>{note.tag}</p>
+          <p className={css.content}>{note.content}</p>
+          <p className={css.date}>
+            {new Date(note.createdAt).toLocaleDateString()}
+          </p>
+          <button className={css.backBtn} onClick={() => router.back()}>
+            Close
+          </button>
         </div>
-        <p className={css.tag}>{note.tag}</p>
-        <p className={css.content}>{note.content}</p>
-        <p className={css.date}>
-          {new Date(note.createdAt).toLocaleDateString()}
-        </p>
       </div>
-    </div>
+    </Modal>
   );
 }
